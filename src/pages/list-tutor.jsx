ec6a90868ce0d9dc3d4977372993cc530a99db18
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-09-09 19:50:21 +0800

    overfl;ow

--- src/pages/list-tutor.jsx
+++ src/pages/list-tutor.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
             <Header title="List Tutor" description="This is the home page" />
-            <div className='min-h-screen bg-primary-100 overflow-hidden'>
+            <div className='min-h-screen bg-primary-100 md:overflow-hidden'>
                 <div className="w-full p-2 lg:px-8 lg:py-4 bg-white shadow-xl sticky top-0 z-50">
                     <MainHeader
                         search={true}
